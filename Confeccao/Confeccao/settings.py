87beade4ba4234c71d70cc8caa609f6c8fa4c896
Denner <dennerbismarck@gmail.com>
2024-02-09 13:40:34 -0300

    Feat: Creating pedido model

--- Confeccao/Confeccao/settings.py
+++ Confeccao/Confeccao/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-!!bw!iw=54)=o$jsdz%5=^bu547gra3325gc)hyqf&zfi%36wb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Confeccao/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- Confeccao/principal/models.py
+++ Confeccao/principal/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Pedido(models.Model):
+    data_entrada = models.DateTimeField(default=timezone.now)
+    nfe = models.CharField(max_length = 10)
+    op = models.CharField(max_length = 10)
+    material = models.CharField(max_lengrh = 10)
+    class Tipo(models.IntegerChoices):
+        M = 0
+        J = 1
+        T = 2
+    tipo = models.IntegerField(choices = Tipo)
+    cliente = models.CharField(max_length = 75)
+    tempo_de_confeccao = models.FloatField()
+    vl_peca = models.DecimalField(decimal_places = 2)
+    vl_frete_pc = models.DecimalField(decimal_places = 2)
+    vl_frete_e = models.DecimalField(decimal_places = 2)
+    qtd_entrada = models.IntegerField()
+    faturamento_a_receber = models.DecimalField(decimal_places = 2)
+    nf_ret = models.CharField()
+    qtd_pecas_retorno = models.IntegerField()
+    total = models.DecimalField(decimal_places = 2)
+    vl_recebido = models.BooleanField(default=False)
+    data_pagamento = models.DateField(blank=True, null=True)
+    tempo_entrada = models.FloatField()
+    artok_e = models.IntegerField()
+    tempo_retorno = models.FloatField()
+    artok_r = models.IntegerField()
+    aliquota = models.FloatField()
+    data_vencimento_pagamento = models.DateField()
+    
