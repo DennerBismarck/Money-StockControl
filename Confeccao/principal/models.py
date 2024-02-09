48769e38f3289243a1bb60746031df467ff265aa
Denner <dennerbismarck@gmail.com>
2024-02-09 14:07:11 -0300

    Fix: DecimalFields max length

--- Confeccao/principal/models.py
+++ Confeccao/principal/models.py
@@ -1,27 +1,26 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
 class Pedido(models.Model):
     data_entrada = models.DateTimeField(default=timezone.now)
-    nfe = models.CharField(max_length = 10)
-    op = models.CharField(max_length = 10)
-    material = models.CharField(max_lengrh = 10)
+    nfe = models.CharField(max_length=10)
+    op = models.CharField(max_length=10)
+    material = models.CharField(max_length=10) 
     class Tipo(models.IntegerChoices):
         M = 0
         J = 1
         T = 2
-    tipo = models.IntegerField(choices = Tipo)
-    cliente = models.CharField(max_length = 75)
+    tipo = models.IntegerField(choices=Tipo.choices) 
+    cliente = models.CharField(max_length=75)
     tempo_de_confeccao = models.FloatField()
-    vl_peca = models.DecimalField(decimal_places = 2)
-    vl_frete_pc = models.DecimalField(decimal_places = 2)
-    vl_frete_e = models.DecimalField(decimal_places = 2)
+    vl_peca = models.DecimalField(decimal_places=2, max_digits=10)  
+    vl_frete_pc = models.DecimalField(decimal_places=2, max_digits=10)  
+    vl_frete_e = models.DecimalField(decimal_places=2, max_digits=10)  
     qtd_entrada = models.IntegerField()
-    faturamento_a_receber = models.DecimalField(decimal_places = 2)
-    nf_ret = models.CharField()
+    faturamento_a_receber = models.DecimalField(decimal_places=2, max_digits=10)  
+    nf_ret = models.CharField(max_length=255)  
     qtd_pecas_retorno = models.IntegerField()
-    total = models.DecimalField(decimal_places = 2)
+    total = models.DecimalField(decimal_places=2, max_digits=10)  
     vl_recebido = models.BooleanField(default=False)
     data_pagamento = models.DateField(blank=True, null=True)
     tempo_entrada = models.FloatField()
@@ -30,4 +29,3 @@ class Pedido(models.Model):
     artok_r = models.IntegerField()
     aliquota = models.FloatField()
     data_vencimento_pagamento = models.DateField()
-    
